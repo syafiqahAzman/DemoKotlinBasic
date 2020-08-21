f6b3c9e9459db4100d4f78a029e12f38420b3c2a
SYAFIQAH BTE AZMAN <16046491@myrp.edu.sg>
2020-08-21 10:22:04 +0800

    Initial commit

---
+++ app/src/main/java/com/myapplicationdev/android/demokotlinbasic/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.myapplicationdev.android.demokotlinbasic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btnGetAge.setOnClickListener{
+            val userDOB = etDOB.text.toString()
+            val year = Calendar.getInstance().get(Calendar.YEAR)
+            val age = year - userDOB.toInt()
+            tvShowAge.text = "Your age is $age"
+        }
+    }
+}
